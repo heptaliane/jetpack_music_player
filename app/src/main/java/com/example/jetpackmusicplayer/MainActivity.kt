df3eee8a62f7970afd6b8397fdc5041b6491046c
heptaliane <heptaliane@gmail.com>
2025-03-24 23:06:42 +0900

    Add Seekbar

--- app/src/main/java/com/example/jetpackmusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/jetpackmusicplayer/MainActivity.kt
@@ -1,47 +1,44 @@
 package com.example.jetpackmusicplayer
 
-import MusicPlayer
 import MusicPlayerScreen
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
-import java.io.File
 
 class MainActivity : ComponentActivity() {
-    private var player: MusicPlayer? = null
-    private var musicFilePath: String = "example.mp3"
+    private var player: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        player = MusicPlayer()
         setContent {
-            val isPlaying by remember { derivedStateOf { player?.isPlaying() == true } }
+            val isPlaying by remember { derivedStateOf { player?.isPlaying == true } }
+            val currentPosition by remember { mutableIntStateOf(0) }
+            val duration by remember { mutableIntStateOf(100) }
+
             MusicPlayerScreen(
                 isPlaying = isPlaying,
+                duration = duration,
+                currentPosition = currentPosition,
                 onResume = { resumeMusic() },
-                onPause = { pauseMusic() }
+                onPause = { pauseMusic() },
+                onSeek = { position ->
+                    player?.seekTo(position)
+                }
             )
         }
     }
 
-    private fun playMusic() {
-        val file = File(musicFilePath)
-        player?.play(file)
-    }
-
-    private fun stopMusic() {
-        player?.stop()
+    private fun resumeMusic() {
+        player?.start()
     }
 
     private fun pauseMusic() {
         player?.pause()
-    }
-
-    private fun resumeMusic() {
-        player?.resume()
     }
 }
--- app/src/main/java/com/example/jetpackmusicplayer/ui/MusicPlayerScreen.kt
+++ app/src/main/java/com/example/jetpackmusicplayer/ui/MusicPlayerScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,8 +13,11 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun MusicPlayerScreen(
     isPlaying: Boolean,
+    currentPosition: Int,
+    duration: Int,
     onPause: () -> Unit,
     onResume: () -> Unit,
+    onSeek: (Int) -> Unit,
 ) {
     Column(
         modifier = Modifier
@@ -21,6 +25,12 @@ fun MusicPlayerScreen(
             .padding(16.dp),
         verticalArrangement = Arrangement.Center
     ) {
+        Slider(
+            value = currentPosition.toFloat(),
+            onValueChange = { onSeek(it.toInt()) },
+            valueRange = 0f..duration.toFloat(),
+            modifier = Modifier.fillMaxWidth(),
+        )
         if (isPlaying) {
             Button(onClick = { onPause() }, modifier = Modifier.fillMaxWidth()) { Text("Pause") }
         } else {
